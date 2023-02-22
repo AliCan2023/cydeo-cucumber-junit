149852f6c3f86ee222bd1ffffd15eda902c76023
Ali Can <alibugraoglu@gmail.com>
2023-02-22 16:04:02 +0300

    day14_DryRun_Tags_HTMLReports

--- src/test/java/com/cydeo/runners/CucuRunner.java
+++ src/test/java/com/cydeo/runners/CucuRunner.java
@@ -7,10 +7,15 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)// this class has to have access to other projects; pass the path of the futures directory;
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = "com/cydeo/step_definitions"
-
+        glue = "com/cydeo/step_definitions",
+        dryRun = false,
+        //tags = "@student"// only one scenario executed;
+        //tags = "@todayIsSunnyDay"// only one scenario executed;
+        tags = "@librarian or @admin "// both of them are executed once checked the condition matches;
 
 )
 
 public class CucuRunner {
+
+
 }
